7f497dd987f7e1fb06fc7635c5108579d6efc93c
JuniorM2702 <Junior_aybar27@hotmail.com>
2024-06-22 21:54:03 -0500

    pregunta 2 terminada (se que no contará, pero quiero terminarlo para aprender

--- src/app/dashboard/pregunta2/pregunta2.component.ts
+++ src/app/dashboard/pregunta2/pregunta2.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-pregunta2',
+  selector: 'app-pregunta1',
   standalone: true,
-  imports: [],
   templateUrl: './pregunta2.component.html',
-  styleUrl: './pregunta2.component.css'
+  styleUrls: ['./pregunta2.component.css'],
+  imports: [MaterialModule, FormsModule, CommonModule],
 })
 export class Pregunta2Component {
+  cantidadLlantas: number = 0;
 
+  precioPorLlantaMenosDe5: number = 800;
+  precioPorLlanta5OMas: number = 700;
+
+  calcularTotal(): number {
+    if (this.cantidadLlantas < 5) {
+      return this.cantidadLlantas * this.precioPorLlantaMenosDe5;
+    } else {
+      return this.cantidadLlantas * this.precioPorLlanta5OMas;
+    }
+  }
 }
