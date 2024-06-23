4beaf8a1408cc5938251576db0b3cf6fed1072e7
JuniorM2702 <Junior_aybar27@hotmail.com>
2024-06-22 21:49:45 -0500

    pregunta 1 terminada

--- src/app/dashboard/pregunta1/pregunta1.component.ts
+++ src/app/dashboard/pregunta1/pregunta1.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta1',
   standalone: true,
-  imports: [],
   templateUrl: './pregunta1.component.html',
-  styleUrl: './pregunta1.component.css'
+  styleUrls: ['./pregunta1.component.css'],
+  imports: [MaterialModule, FormsModule, CommonModule],
 })
 export class Pregunta1Component {
+  precioUnitario: number;
+  cantidad: number;
+  precioFinal: number;
 
+  constructor() {
+    this.precioUnitario = 0; 
+    this.cantidad = 0;
+    this.precioFinal = 0; 
+  }
+
+  calcularPrecio(): void {
+    if (this.cantidad > 20) {
+      this.precioFinal = this.cantidad * this.precioUnitario * 0.9; 
+    } else if (this.cantidad > 10) {
+      this.precioFinal = this.cantidad * this.precioUnitario * 0.95;
+    } else {
+      this.precioFinal = this.cantidad * this.precioUnitario;
+    }
+  }
 }
